3b9722dcc68a6f56c5f48fb680a43065f5dad341
dyoo <dyoo@kfisler-ra1.(none)>
2011-03-03 16:25:34 -0500

    simulator.rkt skeleton

--- simulator.rkt
+++ simulator.rkt
@@ -22,10 +22,40 @@
 
 (: step (machine -> machine))
 (define (step m)
+  (let: ([i : Statement (current-instruction m)])
+        (cond
+          [(symbol? i)
+           (increment-pc m)]
+          [(AssignImmediateStatement? i)
+           (error 'step)]
+          [(AssignPrimOpStatement? i)
+           (error 'step)]
+          [(PerformStatement? i)
+           (error 'step)]
+          [(GotoStatement? i)
+           (error 'step)]
+          [(TestAndBranchStatement? i)
+           (error 'step)]
+          [(PopEnvironment? i)
+           (error 'step)]
+          [(PushEnvironment? i)
+           (error 'step)]
+          [(PushControlFrame? i)
+           (error 'step)]
+          [(PopControlFrame? i)
+           (error 'step)])))
+
+    
+
+
+
+(: current-instruction (machine -> Statement))
+(define (current-instruction m)
   (match m
     [(struct machine (val proc env control pc text))
-     m
-     ]))
+     (vector-ref text pc)]))
+
+  
 
 (: val-update (machine Any -> machine))
 (define (val-update m v)
@@ -74,3 +104,41 @@
      (make-machine val proc (append (take env skip)
                                     (drop env (+ skip n)))
                    control pc text)]))
+
+
+(: control-push (machine Symbol -> machine))
+(define (control-push m l)
+  (match m
+    [(struct machine (val proc env control pc text))
+     (make-machine val proc env (cons (make-frame l) control) pc text)]))
+
+
+(: control-pop (machine Symbol -> (values machine Symbol)))
+(define (control-pop m l)
+  (match m
+    [(struct machine (val proc env control pc text))
+     (values (make-machine val proc env (rest control) pc text)
+             (frame-return (first control)))]))
+
+(: increment-pc (machine -> machine))
+(define (increment-pc m)
+  (match m
+    [(struct machine (val proc env control pc text))
+     (make-machine val proc env control (add1 pc) text)]))
+
+
+(: jump (machine Symbol -> machine))
+(define (jump m l)
+  (match m
+    [(struct machine (val proc env control pc text))
+     (make-machine val proc env control (vector-find text l) text)]))
+
+
+(: vector-find (All (A) (Vectorof A) A -> Natural))
+(define (vector-find vec x)
+  (let: loop : Natural ([i : Natural 0])
+        (cond
+          [(eq? (vector-ref vec i) x)
+           i]
+          [else
+           (loop (add1 i))])))

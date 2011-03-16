8b1172abe30508e0db1ea55ac3284d89be03b7fc
Danny Yoo <dyoo@cs.wpi.edu>
2011-03-16 15:48:30 -0400

    recording the browser in browser-evaluat

--- browser-evaluate.rkt
+++ browser-evaluate.rkt
@@ -20,7 +20,8 @@
 
 
 (define-struct error-happened (str t) #:transparent)
-(define-struct evaluated (stdout value t) #:transparent)
+(define-struct evaluated (stdout value t
+                                 browser) #:transparent)
 
 
 
@@ -139,7 +140,8 @@ EOF
     (channel-put ch (make-evaluated (extract-binding/single 'o (request-bindings req))
                                     (extract-binding/single 'v (request-bindings req))
                                     (string->number
-                                     (extract-binding/single 't (request-bindings req)))))
+                                     (extract-binding/single 't (request-bindings req)))
+                                    (extract-binding/single 'b (request-bindings req))))
     (ok-response))
   
   
@@ -229,7 +231,8 @@ var comet = function() {
                         sendRequest("/eval", function(req) { setTimeout(comet, 0); },
                             "v=" + encodeURIComponent(String(v)) +
                             "&o=" + encodeURIComponent(output.join('')) +
-                            "&t=" + encodeURIComponent(String(endTime - startTime)));
+                            "&t=" + encodeURIComponent(String(endTime - startTime)) +
+                            "&b=" + encodeURIComponent(String(BrowserDetect.browser + ' ' + BrowserDetect.version + '/' + BrowserDetect.OS)));
                     };
 
                     var onFail = function(e) {
@@ -243,6 +246,124 @@ var comet = function() {
                 },
                 "comet=t");
 };
+
+var BrowserDetect = {
+	init: function () {
+		this.browser = this.searchString(this.dataBrowser) || "An unknown browser";
+		this.version = this.searchVersion(navigator.userAgent)
+			|| this.searchVersion(navigator.appVersion)
+			|| "an unknown version";
+		this.OS = this.searchString(this.dataOS) || "an unknown OS";
+	},
+	searchString: function (data) {
+		for (var i=0;i<data.length;i++)	{
+			var dataString = data[i].string;
+			var dataProp = data[i].prop;
+			this.versionSearchString = data[i].versionSearch || data[i].identity;
+			if (dataString) {
+				if (dataString.indexOf(data[i].subString) != -1)
+					return data[i].identity;
+			}
+			else if (dataProp)
+				return data[i].identity;
+		}
+	},
+	searchVersion: function (dataString) {
+		var index = dataString.indexOf(this.versionSearchString);
+		if (index == -1) return;
+		return parseFloat(dataString.substring(index+this.versionSearchString.length+1));
+	},
+	dataBrowser: [
+		{
+			string: navigator.userAgent,
+			subString: "Chrome",
+			identity: "Chrome"
+		},
+		{ 	string: navigator.userAgent,
+			subString: "OmniWeb",
+			versionSearch: "OmniWeb/",
+			identity: "OmniWeb"
+		},
+		{
+			string: navigator.vendor,
+			subString: "Apple",
+			identity: "Safari",
+			versionSearch: "Version"
+		},
+		{
+			prop: window.opera,
+			identity: "Opera"
+		},
+		{
+			string: navigator.vendor,
+			subString: "iCab",
+			identity: "iCab"
+		},
+		{
+			string: navigator.vendor,
+			subString: "KDE",
+			identity: "Konqueror"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "Firefox",
+			identity: "Firefox"
+		},
+		{
+			string: navigator.vendor,
+			subString: "Camino",
+			identity: "Camino"
+		},
+		{		// for newer Netscapes (6+)
+			string: navigator.userAgent,
+			subString: "Netscape",
+			identity: "Netscape"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "MSIE",
+			identity: "Explorer",
+			versionSearch: "MSIE"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "Gecko",
+			identity: "Mozilla",
+			versionSearch: "rv"
+		},
+		{ 		// for older Netscapes (4-)
+			string: navigator.userAgent,
+			subString: "Mozilla",
+			identity: "Netscape",
+			versionSearch: "Mozilla"
+		}
+	],
+	dataOS : [
+		{
+			string: navigator.platform,
+			subString: "Win",
+			identity: "Windows"
+		},
+		{
+			string: navigator.platform,
+			subString: "Mac",
+			identity: "Mac"
+		},
+		{
+			   string: navigator.userAgent,
+			   subString: "iPhone",
+			   identity: "iPhone/iPod"
+	    },
+		{
+			string: navigator.platform,
+			subString: "Linux",
+			identity: "Linux"
+		}
+	]
+
+};
+BrowserDetect.init();
+
 
 var whenLoaded = function() {
     setTimeout(comet, 0);

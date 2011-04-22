b746240d7716a818a8b3e01a1fa41290107be723
Danny Yoo <dyoo@cs.wpi.edu>
2011-04-22 13:01:34 -0400

    added man-or-boy test

--- test-browser-evaluate.rkt
+++ test-browser-evaluate.rkt
@@ -481,7 +481,22 @@ EOF
       
 
 
-
+;; Knuth's Man-or-boy-test.
+;; http://rosettacode.org/wiki/Man_or_boy_test
+(test '(begin (define (A k x1 x2 x3 x4 x5)
+		(letrec ([B (lambda ()
+			   (set! k (- k 1))
+			   (A k B x1 x2 x3 x4))])
+		  (if (<= k 0)
+		      (+ (x4) (x5))
+		      (B))))
+	      (displayln (A 10
+			    (lambda () 1) 
+			    (lambda () -1) 
+			    (lambda () -1)
+			    (lambda () 1) 
+			    (lambda () 0))))
+      "-67\n")
 
 
 

56e523fbc99e5fbe079e01dd60c6ef214faf0cc8
Danny Yoo <dyoo@cs.wpi.edu>
2011-07-01 11:33:46 -0400

    continuing to work on structures

--- tests/more-tests/hello.rkt
+++ tests/more-tests/hello.rkt
@@ -1,2 +1,8 @@
 #lang planet dyoo/whalesong
 (displayln "hello world")
+
+
+(displayln (format "hello ~a" "again"))
+
+(printf "hello")
+(printf "world\n")
--- tests/more-tests/simple-structs.rkt
+++ tests/more-tests/simple-structs.rkt
@@ -3,19 +3,14 @@
 (define-struct p (f r))
 
 
-"1"
 struct:p
-"2"
 make-p
-"3"
 p-f
-"4"
 p-r
-"5"
 p?
-"6"
 
 
-#;(define p1 (make-p 3 4))
-#;(p-f p1)
-#;(p-r p1)
+
+(define p1 (make-p 3 4))
+(p-f p1)
+(p-r p1)

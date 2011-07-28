c7dd5cc99c9d6db15b6972015471e6ce3a3fa785
Danny Yoo <dyoo@cs.wpi.edu>
2011-07-28 16:39:21 -0400

    trying to trace why apply isn't working with regards to a simple definition

--- tests/more-tests/simple-apply.rkt
+++ tests/more-tests/simple-apply.rkt
@@ -1,7 +1,46 @@
 #lang planet dyoo/whalesong
 
 (apply + 2 3 4 5 '())
+(apply + 2 3 4 '(5))
+(apply + 2 3 '(4 5))
+(apply + 2 '(3 4 5))
+(apply + '(2 3 4 5))
+
+
+(apply * 2 3 4 5 '())
+(apply * 2 3 4 '(5))
+(apply * 2 3 '(4 5))
+(apply * 2 '(3 4 5))
+(apply * '(2 3 4 5))
+
 (apply (lambda args args) 2 3 4 5 '())
+(apply (lambda args args) 2 3 4 '(5))
+(apply (lambda args args) 2 3 '(4 5))
+(apply (lambda args args) 2 '(3 4 5))
+(apply (lambda args args) '(2 3 4 5))
+
+
+(apply (lambda (a b c) (+ a (* b c))) 2 3 '(4))
+(add1 (apply (lambda (a b c) (+ a (* b c))) 2 3 '(4)))
+(= (apply (lambda (a b c) (+ a (* b c))) 2 3 '(4))
+   0)
+
+(apply list 3 4 '(5))
+
+
+(define (square x)
+  (* x x))
+
+(apply square '(7))
+
+"squaring"
+
+(define (square2 x)
+  (apply * (list x x)))
+
+(apply square2 '(7))
+(apply square2 8 '())
+
 
 "now factorial"
 
@@ -12,6 +51,10 @@
    [else
     (apply * `(,x ,(apply f (apply sub1 (apply list x '())) '())))]))
 
-
-(f 3)
-(+ (f 4) (f 5))
+(f 0)
+;(f 1)
+;(f 2)
+;(f 3)
+;(f 4)
+;(f 5)
+;(+ (f 4) (f 5))

5c5ef8b518d15eba71dd7b20ac45f3e447b42e7b
Danny Yoo <dyoo@cs.wpi.edu>
2011-08-08 00:28:35 -0400

    more jslinting

--- js-assembler/runtime-src/baselib-check.js
+++ js-assembler/runtime-src/baselib-check.js
@@ -1,56 +1,82 @@
+/*jslint vars: true, white: true, plusplus: true, maxerr: 50, indent: 4 */
+
+
 // Helper functions for argument checking.
 
-(function(baselib) {
+(function (baselib) {
     'use strict';
     var exports = {};
     baselib.check = exports;
 
-    var EMPTY = plt.baselib.lists.EMPTY;
-    var isPair = plt.baselib.lists.isPair;
-    var makeLowLevelEqHash = plt.baselib.hashes.makeLowLevelEqHash;
+    var EMPTY = baselib.lists.EMPTY;
+    var isPair = baselib.lists.isPair;
+    var makeLowLevelEqHash = baselib.hashes.makeLowLevelEqHash;
 
 
     //////////////////////////////////////////////////////////////////////
 
-    var makeCheckArgumentType = function(predicate, predicateName) {
-	return function(MACHINE, callerName, position) {
-	    testArgument(
-		MACHINE,
-		predicateName,
-		predicate,
-		MACHINE.env[MACHINE.env.length - 1 - position],
-		position,
-		callerName);
-	    return MACHINE.env[MACHINE.env.length - 1 - position];
-	}
+    // testArgument: (X -> boolean) X number string string -> boolean
+    // Produces true if val is true, and otherwise raises an error.
+    var testArgument = function (MACHINE,
+                                 expectedTypeName,
+                                 predicate,                          
+                                 val, 
+                                 index, 
+                                 callerName) {
+        if (predicate(val)) {
+            return true;
+        } else {
+            if (typeof(expectedTypeName) === 'function') { 
+                expectedTypeName = expectedTypeName(); 
+            }
+            baselib.exceptions.raiseArgumentTypeError(MACHINE, 
+                                                      callerName,
+                                                      expectedTypeName,
+                                                      index,
+                                                      val);
+        }
     };
 
-    var makeCheckParameterizedArgumentType = function(parameterizedPredicate, 
-                                                      parameterizedPredicateName) {
-	return function(MACHINE, callerName, position) {
-            var args = [];
-            for (var i = 3; i < arguments.length; i++) {
+
+    var makeCheckArgumentType = function (predicate, predicateName) {
+        return function (MACHINE, callerName, position) {
+            testArgument(
+                MACHINE,
+                predicateName,
+                predicate,
+                MACHINE.env[MACHINE.env.length - 1 - position],
+                position,
+                callerName);
+            return MACHINE.env[MACHINE.env.length - 1 - position];
+        };
+    };
+
+    var makeCheckParameterizedArgumentType = function (parameterizedPredicate, 
+                                                       parameterizedPredicateName) {
+        return function (MACHINE, callerName, position) {
+            var args = [], i;
+            for (i = 3; i < arguments.length; i++) {
                 args.push(arguments[i]);
             }
-	    testArgument(
-		MACHINE,
-		function() { parameterizedPredicateName.apply(null, args) },
-		function(x) {
+            testArgument(
+                MACHINE,
+                function () { parameterizedPredicateName.apply(null, args); },
+                function (x) {
                     return parameterizedPredicate.apply(null, [x].concat(args));
                 },
-		MACHINE.env[MACHINE.env.length - 1 - position],
-		position,
-		callerName);
-	    return MACHINE.env[MACHINE.env.length - 1 - position];
-	}
+                MACHINE.env[MACHINE.env.length - 1 - position],
+                position,
+                callerName);
+            return MACHINE.env[MACHINE.env.length - 1 - position];
+        };
     };
 
 
 
 
 
-    var makeCheckListofArgumentType = function(predicate, predicateName) {
-        var listPredicate = function(x) {
+    var makeCheckListofArgumentType = function (predicate, predicateName) {
+        var listPredicate = function (x) {
             var seen = makeLowLevelEqHash();
             while (true) {
                 if (x === EMPTY){
@@ -63,7 +89,7 @@
 
                 if(seen.containsKey(x)) {
                     // raise an error? we've got a cycle!
-                    return false
+                    return false;
                 }
 
                 if (! predicate(x.first)) {
@@ -74,16 +100,16 @@
                 x = x.rest;
             }
         };
-	return function(MACHINE, callerName, position) {
-	    testArgument(
-		MACHINE,
-		'list of ' + predicateName,
-		listPredicate,
-		MACHINE.env[MACHINE.env.length - 1 - position],
-		position,
-		callerName);
-	    return MACHINE.env[MACHINE.env.length - 1 - position];
-	}
+        return function (MACHINE, callerName, position) {
+            testArgument(
+                MACHINE,
+                'list of ' + predicateName,
+                listPredicate,
+                MACHINE.env[MACHINE.env.length - 1 - position],
+                position,
+                callerName);
+            return MACHINE.env[MACHINE.env.length - 1 - position];
+        };
     };
 
 
@@ -91,135 +117,111 @@
 
 
 
-    // testArgument: (X -> boolean) X number string string -> boolean
-    // Produces true if val is true, and otherwise raises an error.
-    var testArgument = function(MACHINE,
-				expectedTypeName,
-				predicate, 			    
-				val, 
-				index, 
-				callerName) {
-	if (predicate(val)) {
-	    return true;
-	} else {
-            if (typeof(expectedTypeName) === 'function') { 
-                expectedTypeName = expectedTypeName(); 
-            }
-	    plt.baselib.exceptions.raiseArgumentTypeError(MACHINE, 
-                                                          callerName,
-                                                          expectedTypeName,
-				                          index,
-				                          val);
-	}
-    };
 
-    var testArity = function(callerName, observed, minimum, maximum) {
-	if (observed < minimum || observed > maximum) {
-	    plt.baselib.exceptions.raise(
+    var testArity = function (MACHINE, callerName, observed, minimum, maximum) {
+        if (observed < minimum || observed > maximum) {
+            baselib.exceptions.raise(
                 MACHINE, new Error(callerName + ": expected at least " + minimum
-				   + " arguments "
-				   + " but received " + observed));
+                                   + " arguments "
+                                   + " but received " + observed));
 
-	}
+        }
     };
 
 
 
 
     var checkOutputPort = makeCheckArgumentType(
-        plt.baselib.ports.isOutputPort,
+        baselib.ports.isOutputPort,
         'output port');
 
     var checkSymbol = makeCheckArgumentType(
-	plt.baselib.symbols.isSymbol,
-	'symbol');
+        baselib.symbols.isSymbol,
+        'symbol');
 
     var checkString = makeCheckArgumentType(
-        plt.baselib.strings.isString,
+        baselib.strings.isString,
         'string');
 
     var checkMutableString = makeCheckArgumentType(
-        plt.baselib.strings.isMutableString,
+        baselib.strings.isMutableString,
         'mutable string');
 
     var checkChar = makeCheckArgumentType(
-        plt.baselib.chars.isChar,
+        baselib.chars.isChar,
         'character');
 
     var checkProcedure = makeCheckArgumentType(
-        plt.baselib.functions.isProcedure,
+        baselib.functions.isProcedure,
         'procedure');
 
     var checkNumber = makeCheckArgumentType(
-        plt.baselib.numbers.isNumber,
+        baselib.numbers.isNumber,
         'number');
 
     var checkReal = makeCheckArgumentType(
-        plt.baselib.numbers.isReal,
+        baselib.numbers.isReal,
         'real');
 
     var checkNatural = makeCheckArgumentType(
-        plt.baselib.numbers.isNatural,
+        baselib.numbers.isNatural,
         'natural');
 
     var checkByte = makeCheckArgumentType(
-        function(x) { return (typeof(x) === 'number' && 0 <= x && x < 256) },
+        baselib.numbers.isByte,
         'byte');
 
     var checkNaturalInRange = makeCheckParameterizedArgumentType(
-        function(x, a, b) {
-            if (! plt.baselib.numbers.isNatural(x)) { return false; }
-            return (plt.baselib.numbers.lessThanOrEqual(a, x) &&
-                    plt.baselib.numbers.lessThan(x, b));
+        function (x, a, b) {
+            if (! baselib.numbers.isNatural(x)) { return false; }
+            return (baselib.numbers.lessThanOrEqual(a, x) &&
+                    baselib.numbers.lessThan(x, b));
         },
-        function(a, b) {
-            return plt.baselib.format.format('natural between ~a and ~a', [a, b]);
+        function (a, b) {
+            return baselib.format.format('natural between ~a and ~a', [a, b]);
         });
 
     var checkInteger = makeCheckArgumentType(
-        plt.baselib.numbers.isInteger,
+        baselib.numbers.isInteger,
         'integer');
 
     var checkRational = makeCheckArgumentType(
-        plt.baselib.numbers.isRational,
+        baselib.numbers.isRational,
         'rational');
 
     var checkNonNegativeReal = makeCheckArgumentType(
-        plt.baselib.numbers.isNonNegativeReal,
+        baselib.numbers.isNonNegativeReal,
         'non-negative real');
 
     var checkPair = makeCheckArgumentType(
-        plt.baselib.lists.isPair,
+        baselib.lists.isPair,
         'pair');
 
     var checkList = makeCheckArgumentType(
-        plt.baselib.lists.isList,
+        baselib.lists.isList,
         'list');
 
     var checkVector = makeCheckArgumentType(
-        plt.baselib.vectors.isVector,
+        baselib.vectors.isVector,
         'vector');
 
     var checkBoolean = makeCheckArgumentType(
-        function(x) { return x === true || x === false; },
+        function (x) { return x === true || x === false; },
         'boolean');
 
     var checkBox = makeCheckArgumentType(
-        plt.baselib.boxes.isBox,
+        baselib.boxes.isBox,
         'box');
 
     var checkMutableBox = makeCheckArgumentType(
-        plt.baselib.boxes.isMutableBox,
+        baselib.boxes.isMutableBox,
         'mutable box');
 
     var checkInspector = makeCheckArgumentType(
-        plt.baselib.inspectors.isInspector,
+        baselib.inspectors.isInspector,
         'inspector');
 
 
-    var checkByte = makeCheckArgumentType(
-        plt.baselib.numbers.isByte,
-        'byte');
 
 
 

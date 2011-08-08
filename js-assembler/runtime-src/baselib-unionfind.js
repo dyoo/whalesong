5971577d397fc6609628cd1a159d2614107569f3
Danny Yoo <dyoo@cs.wpi.edu>
2011-08-07 21:13:43 -0400

    starting to run through jslint

--- js-assembler/runtime-src/baselib-unionfind.js
+++ js-assembler/runtime-src/baselib-unionfind.js
@@ -1,41 +1,38 @@
-(function(baselib) {
-
-
-
-
-
+/*jslint devel: false, browser: true, vars: true, plusplus: true, maxerr: 500, indent: 4 */
+(function (baselib) {
+    "use strict";
 
     // Union/find for circular equality testing.
 
-    var UnionFind = function() {
-	// this.parenMap holds the arrows from an arbitrary pointer
-	// to its parent.
-	this.parentMap = baselib.hashes.makeLowLevelEqHash();
-    }
+    var UnionFind = function () {
+        // this.parenMap holds the arrows from an arbitrary pointer
+        // to its parent.
+        this.parentMap = baselib.hashes.makeLowLevelEqHash();
+    };
 
     // find: ptr -> UnionFindNode
     // Returns the representative for this ptr.
-    UnionFind.prototype.find = function(ptr) {
-	var parent = (this.parentMap.containsKey(ptr) ? 
-		      this.parentMap.get(ptr) : ptr);
-	if (parent === ptr) {
-	    return parent;
-	} else {
-	    var rep = this.find(parent);
-	    // Path compression:
-	    this.parentMap.put(ptr, rep);
-	    return rep;
-	}
+    UnionFind.prototype.find = function (ptr) {
+        var parent = (this.parentMap.containsKey(ptr) ? 
+                      this.parentMap.get(ptr) : ptr);
+        if (parent === ptr) {
+            return parent;
+        } else {
+            var rep = this.find(parent);
+            // Path compression:
+            this.parentMap.put(ptr, rep);
+            return rep;
+        }
     };
 
     // merge: ptr ptr -> void
     // Merge the representative nodes for ptr1 and ptr2.
-    UnionFind.prototype.merge = function(ptr1, ptr2) {
-	this.parentMap.put(this.find(ptr1), this.find(ptr2));
+    UnionFind.prototype.merge = function (ptr1, ptr2) {
+        this.parentMap.put(this.find(ptr1), this.find(ptr2));
     };
 
 
 
     baselib.UnionFind = UnionFind;
 
-})(this['plt'].baselib);
+}(this.plt.baselib));
--- js-assembler/runtime-src/baselib-vectors.js
+++ js-assembler/runtime-src/baselib-vectors.js
@@ -1,104 +1,108 @@
 // vectors
-(function(baselib) {
+/*jslint devel: false, browser: true, vars: true, plusplus: true, maxerr: 500, indent: 4 */
+(function (baselib) {
+    "use strict";
     var exports = {};
     baselib.vectors = exports;
 
 
 
-    var Vector = function(n, initialElements) {
-        this.elts = new Array(n);
+    var Vector = function (n, initialElements) {
+        var i;
+        this.elts = [];
         if (initialElements) {
-	    for (var i = 0; i < n; i++) {
-	        this.elts[i] = initialElements[i];
-	    }
+            for (i = 0; i < n; i++) {
+                this.elts[i] = initialElements[i];
+            }
         } else {
-	    for (var i = 0; i < n; i++) {
-	        this.elts[i] = undefined;
-	    }
+            for (i = 0; i < n; i++) {
+                this.elts[i] = undefined;
+            }
         }
         this.mutable = true;
     };
 
-    Vector.makeInstance = function(n, elts) {
+    Vector.makeInstance = function (n, elts) {
         return new Vector(n, elts);
-    }
-
-    Vector.prototype.length = function() {
-	return this.elts.length;
     };
 
-    Vector.prototype.ref = function(k) {
+    Vector.prototype.length = function () {
+        return this.elts.length;
+    };
+
+    Vector.prototype.ref = function (k) {
         return this.elts[k];
     };
 
-    Vector.prototype.set = function(k, v) {
+    Vector.prototype.set = function (k, v) {
         this.elts[k] = v;
     };
 
-    Vector.prototype.equals = function(other, aUnionFind) {
-        if (other != null && other != undefined && other instanceof Vector) {
-	    if (other.length() != this.length()) {
-	        return false
-	    }
-	    for (var i = 0; i <  this.length(); i++) {
-	        if (! plt.baselib.equality.equals(this.elts[i], other.elts[i], aUnionFind)) {
-		    return false;
-	        }
-	    }
-	    return true;
+    Vector.prototype.equals = function (other, aUnionFind) {
+        var i;
+        if (other instanceof Vector) {
+            if (other.length() !== this.length()) {
+                return false;
+            }
+            for (i = 0; i <  this.length(); i++) {
+                if (!(baselib.equality.equals(this.elts[i], other.elts[i], aUnionFind))) {
+                    return false;
+                }
+            }
+            return true;
         } else {
-	    return false;
+            return false;
         }
     };
 
-    Vector.prototype.toList = function() {
-        var ret = plt.baselib.lists.EMPTY;
-        for (var i = this.length() - 1; i >= 0; i--) {
-	    ret = plt.baselib.lists.Cons.makeInstance(this.elts[i], ret);	    
-        }	
+    Vector.prototype.toList = function () {
+        var ret = baselib.lists.EMPTY, i;
+        for (i = this.length() - 1; i >= 0; i--) {
+            ret = baselib.lists.Cons.makeInstance(this.elts[i], ret);           
+        }       
         return ret;
     };
 
-    Vector.prototype.toWrittenString = function(cache) {
+    Vector.prototype.toWrittenString = function (cache) {
+        var texts = [], i;
         cache.put(this, true);
-        var texts = [];
-        for (var i = 0; i < this.length(); i++) {
-	    texts.push(plt.baselib.format.toWrittenString(this.ref(i), cache));
+        for (i = 0; i < this.length(); i++) {
+            texts.push(baselib.format.toWrittenString(this.ref(i), cache));
         }
         return "#(" + texts.join(" ") + ")";
     };
 
-    Vector.prototype.toDisplayedString = function(cache) {
+    Vector.prototype.toDisplayedString = function (cache) {
+        var texts = [], i;
         cache.put(this, true);
-        var texts = [];
-        for (var i = 0; i < this.length(); i++) {
-	    texts.push(plt.baselib.format.toDisplayedString(this.ref(i), cache));
+        for (i = 0; i < this.length(); i++) {
+            texts.push(baselib.format.toDisplayedString(this.ref(i), cache));
         }
         return "#(" + texts.join(" ") + ")";
     };
 
-    Vector.prototype.toDomNode = function(cache) {
+    Vector.prototype.toDomNode = function (cache) {
+        var node = document.createElement("span"), i;
         cache.put(this, true);
-        var node = document.createElement("span");
         node.appendChild(document.createTextNode("#("));
-        for (var i = 0; i < this.length(); i++) {
-	    node.appendChild(plt.baselib.format.toDomNode(this.ref(i), cache));
-	    if (i !== this.length()-1) {
-	        node.appendChild(document.createTextNode(" "));
-	    }
+        for (i = 0; i < this.length(); i++) {
+            node.appendChild(baselib.format.toDomNode(this.ref(i), cache));
+            if (i !== this.length() - 1) {
+                node.appendChild(document.createTextNode(" "));
+            }
         }
         node.appendChild(document.createTextNode(")"));
         return node;
     };
 
 
-    var isVector = function(x) { return x instanceof Vector; };
+    var isVector = function (x) { return x instanceof Vector; };
 
-    var makeVector = function() {
+    var makeVector = function () {
         return Vector.makeInstance(arguments.length, arguments);
     };
 
-    var makeVectorImmutable = function() {
+    var makeVectorImmutable = function () {
         var v = Vector.makeInstance(arguments.length, arguments);
         v.mutable = false;
         return v;
@@ -114,4 +118,4 @@
     exports.makeVectorImmutable = makeVectorImmutable;
 
 
-})(this['plt'].baselib);
+}(this.plt.baselib));

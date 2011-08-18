77bb781c8dc12bd8a229edd6e717ae1709c4b398
Danny Yoo <dyoo@cs.wpi.edu>
2011-08-18 16:38:10 -0400

    releasing another alpha version

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define name "Whalesong")
 (define blurb '("A Racket to JavaScript compiler"))
 (define release-notes '((p "A not-even-alpha release; please don't use this unless you expect sharp edges...")))
-(define version "0.02")
+(define version "0.03")
 (define categories '(devtools))
 (define repositories '("4.x"))
 (define required-core-version "5.1.1")

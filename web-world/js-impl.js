ca333749fe4fe7a2ac9556a970d46a04d4fe6695
Danny Yoo <dyoo@cs.wpi.edu>
2011-08-22 15:06:09 -0400

    continuing to mock the implementation

--- web-world/js-impl.js
+++ web-world/js-impl.js
@@ -1,3 +1,43 @@
-EXPORTS['big-bang'] = undefined;
-EXPORTS['initial-view'] = undefined;
-EXPORTS['stop-when'] = undefined;
+var makePrimitiveProcedure = plt.baselib.functions.makePrimitiveProcedure;
+var makeClosure = plt.baselib.functions.makeClosure;
+var finalizeClosureCall = plt.baselib.functions.finalizeClosureCall;
+var PAUSE = plt.runtime.PAUSE;
+
+
+
+EXPORTS['big-bang'] = makeClosure(
+    'big-bang',
+    plt.baselib.arity.makeArityAtLeast(1),
+    function(MACHINE) {
+        var oldArgcount = MACHINE.argcount;
+
+        PAUSE(function(restart) {
+
+
+
+
+            restart(function(MACHINE) {
+                MACHINE.argcount = oldArgcount;
+                finalizeClosureCall(MACHINE, "ok");
+            })});
+    });
+
+
+
+EXPORTS['initial-view'] = makePrimitiveProcedure(
+    'initial-view',
+    1,
+    function(MACHINE) {
+        return undefined;
+    });
+
+
+EXPORTS['stop-when'] = makePrimitiveProcedure(
+    'stop-when',
+    1,
+    function(MACHINE) {
+        return undefined;
+    });
+
+
+//////////////////////////////////////////////////////////////////////

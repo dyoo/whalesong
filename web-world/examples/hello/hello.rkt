e9a951f04da1e46673e76a7855ffcaf17e947350
Danny Yoo <dyoo@cs.wpi.edu>
2011-08-22 17:27:36 -0400

    getting hello world to do something

--- web-world/examples/hello/hello.rkt
+++ web-world/examples/hello/hello.rkt
@@ -2,18 +2,8 @@
 (require (planet dyoo/whalesong/web-world)
          (planet dyoo/whalesong/resource))
 
-
 (define-resource index.html)
 (define-resource style.css)
-
-;; (printf "I see the path is: ~s\n"
-;;         (resource-path index.html))
-
-;; (printf "I see the key is: ~s\n"
-;;         (resource-key index.html))
-
-;; (printf "I see the content is: ~s\n"
-;;         (resource-content index.html))
 
 (big-bang "don't care"
           (initial-view index.html))

212d23a142ec0f7a53e24d12e78243d62a39b8f4
Danny Yoo <dyoo@cs.wpi.edu>
2011-08-25 17:05:37 -0400

    got a reasonable attr-based animation in place

--- web-world/examples/attr-animation/attr-animation.rkt
+++ web-world/examples/attr-animation/attr-animation.rkt
@@ -9,25 +9,38 @@
 (define (tick w v)
   (modulo (add1 w) 10))
 
-(define (draw w v)
-  (define base (update-view-attr (view-focus v (format "#~a" w))
-                                 "class"
-                                 "selectedBlock"))
-  (define with-left (if (> w 0)
-                        (view-right (update-view-attr (view-left base)
-                                                      "class"
-                                                      "offsetBlock"))
-                        base))
-  (define with-right (if (< w 9)
-                         (view-left (update-view-attr (view-right base)
-                                                      "class"
-                                                      "offsetBlock"))
-                         with-left))
-  with-right)
 
+;; pick-block: world view -> view
+;; Focus the view on block i.
+(define (pick-block v i)
+  (view-focus v (format "#~a" i)))
+
+
+(define (draw w v)
+  (define v1 (update-view-attr
+              (pick-block v w)
+              "class"
+              "selectedBlock"))
+  (define v2 (update-view-attr
+              (pick-block v1 (modulo (sub1 w) 10))
+              "class"
+              "offsetBlock"))
+  (define v3 (update-view-attr
+              (pick-block v2 (modulo (add1 w) 10))
+              "class"
+              "offsetBlock"))
+  (define v4 (update-view-attr
+              (pick-block v3 (modulo (- w 2) 10))
+                          "class"
+                          "block"))
+  (define v5 (update-view-attr
+              (pick-block v4 (modulo (+ w 2) 10))
+              "class"
+              "block"))
+  v5)
 
 
 (big-bang 0
           (initial-view index.html)
-          (on-tick tick 1/2)
+          (on-tick tick)
           (to-draw draw))

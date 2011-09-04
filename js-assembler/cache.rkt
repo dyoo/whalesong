77fc448ed9ae3953fe4c9e3d161b5cdb4f8444bd
Danny Yoo <dyoo@cs.wpi.edu>
2011-09-03 21:40:50 -0400

    going to institute a simple cache

---
+++ js-assembler/cache.rkt
@@ -0,0 +1,7 @@
+#lang racket/base
+(define-struct cached-entry (real-path ;; path to a module. 
+                             whalesong-version ;; string
+                             md5   ;; md5 of the original source in real-path
+                             bytes)
+  #:transparent) ;; bytes
+
--- js-assembler/package.rkt
+++ js-assembler/package.rkt
@@ -52,6 +52,13 @@
                     (void))))
 
 
+(define-struct cached-entry (real-path ;; path to a module. 
+                             whalesong-version ;; string
+                             md5   ;; md5 of the original source in real-path
+                             bytes)
+  #:transparent) ;; bytes
+
+
 
 
 (define-struct js-impl (name ;; symbol

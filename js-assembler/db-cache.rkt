51be2041db073ba2afb74ec9930aae61404652bf
Danny Yoo <dyoo@cs.wpi.edu>
2011-09-23 11:39:57 -0400

    db-cacheing

--- js-assembler/db-cache.rkt
+++ js-assembler/db-cache.rkt
@@ -1,18 +1,43 @@
 #lang racket/base
 
 (require (planet ryanc/db)
-	 "../version.rkt")
+         (prefix-in whalesong: "../version.rkt")
+         racket/file
+         racket/path)
+
+(define cache-directory-path
+  (build-path (find-system-path 'pref-dir)
+              "whalesong"))
 
 
-(when (not (file-exists? "whalesong-cache.sqlite3"))
-   (define conn 
-     (sqlite3-connect #:database "whalesong-cache.sqlite3"
+
+;; create-cache-directory!: -> void
+(define (create-cache-directory!)
+  (unless (directory-exists? cache-directory-path)
+    (make-directory* cache-directory-path)))
+  
+(create-cache-directory!)
+
+(define whalesong-cache.sqlite3 
+  (build-path cache-directory-path 
+              (format "whalesong-cache-~a.sqlite"
+                      whalesong:version)))
+
+
+(when (not (file-exists? whalesong-cache.sqlite3))
+  (define conn 
+     (sqlite3-connect #:database whalesong-cache.sqlite3
 		      #:mode 'create))
-   (disconnect conn))
+  (query-exec conn
+              (string-append
+               "create table cache(path string not null primary key, "
+               " md5sum string not null, "
+               "data blob not null);"))
+  (disconnect conn))
  
 
 (define conn 
-  (sqlite3-connect #:database "whalesong-cache.sqlite3"))
+  (sqlite3-connect #:database whalesong-cache.sqlite3))
 
 
 (define (cached? path)
--- version.rkt
+++ version.rkt
@@ -6,4 +6,4 @@
 
 (provide version)
 (: version String)
-(define version "1.8")
+(define version "1.10")

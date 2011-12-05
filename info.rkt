8aeaa902c938a4b1b94ba768abcacf21a69a6f2a
Danny Yoo <dyoo@cs.wpi.edu>
2011-12-04 20:53:31 -0500

    updating info.rkt

--- info.rkt
+++ info.rkt
@@ -2,8 +2,8 @@
 
 (define name "Whalesong")
 (define blurb '("A Racket to JavaScript compiler"))
-(define release-notes '((p "Identified an issue with the inliner that causes generated code to be much larger than expected.  Inlining is now off until I identify ths issue.")))
-(define version "1.10")
+(define release-notes '((p "Bug patch: under rare situations, the scheduler can incorrectly allow  multiple computations to run.  For now, Whalesong's will not release control to the browser during long-running computations.  Program output may be a little bit more delayed as a result.")))
+(define version "1.11")
 (define categories '(devtools))
 (define repositories '("4.x"))
 (define required-core-version "5.1.1")

433a73aefab4f0cff71d219f2494047e6df915b9
Danny Yoo <dyoo@hashcollision.org>
2012-10-23 17:45:47 -0600

    trying to eliminate use of Any; something about it is breaking with typed racket in 5.3.1

--- parser/where-is-collects.rkt
+++ parser/where-is-collects.rkt
@@ -1,18 +1,20 @@
 #lang typed/racket/base
 (require/typed racket/path
                (normalize-path (Path -> Path)))
-(require/typed typed/racket/base
-               (relative-path? (Any -> Boolean))
-               (find-executable-path (Path Path -> Path)))
 
 (provide collects-path)
 
 (define collects-path
   (normalize-path
-   (let ([p (find-system-path 'collects-dir)])
+   (let: ([p : Path (find-system-path 'collects-dir)])
      (cond
       [(relative-path? p)
-       (find-executable-path (find-system-path 'exec-file)
-                             (find-system-path 'collects-dir))]
+       (define maybe-path (find-executable-path (find-system-path 'exec-file)
+                                                (find-system-path 'collects-dir)))
+       (cond
+         [(path? maybe-path)
+          maybe-path]
+         [else
+          (error 'collects-path "Could not find collects path")])]
       [else
        p]))))
